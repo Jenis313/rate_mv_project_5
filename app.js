702e89f61865f2892687c5a8a5e403fb0ffc78c1
Jenis Rai <jenishrai313@gmail.com>
2021-09-22 15:14:23 +1000

    misc change

--- app.js
+++ app.js
@@ -1,11 +1,50 @@
 const express = require('express');
+const path = require('path')
 const app = express();
-const PORT = 3131; //Can be changed as per your machine condition
+const PORT = 3131; //Can be changed as per your machine's situation
 
-app.get('/', (req, res, next) => {
-    res.send('welcome!')
-})
+// Load routers
+const indexRouter = require('./controllers/index.controller')
 
+// Dev Tool
+var logger = require('morgan');
+app.use(logger('dev'));
+
+// view engine setup
+app.set('view engine', 'ejs');
+
+// parsers
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//serve static files
+app.use(express.static('public'));
+
+// Routes
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next({
+    msg: 'Page not found',
+    status: 404
+  });
+});
+// error handler middleware
+app.use(function(err, req, res, next) {
+  console.log('ERROR Handling middleware in execution!!! --> ', err);
+
+  res.status(err.status || 500);
+  res.json({
+    msg: err.msg || err,
+    status: err.status || 404
+  })
+
+//   // render the error page
+//   res.render('./pages/error', {
+//     msg: err.msg || err
+//   });
+});
 
 app.listen(PORT, (err) => {
     if(err){
