9545c7c51835bac7dbd7a3baf0bb3459d12fe61c
vhayne14 <vhayne14@gmail.com>
2021-09-24 13:40:38 +1000

    Moved index js to routes

---
+++ controllers/signup.controller.js
@@ -0,0 +1,59 @@
+
+const express = require('express')
+const bcrypt = require('bcryptjs')
+const router = express.Router()
+const db = require('./../models/db.config');
+const signupRouter = require('./../routes/index')
+
+
+
+router.get('/',(req,res)=>{
+    
+// console.log(req.session);
+//     res.send({
+//         title: 'Signup page'
+//     })
+
+    res.render('pages/signup', {
+        message: req.query.message
+  })
+})
+
+router.post("/",(req,res) => {
+    // 1. validate user data (try joi package)
+    const { firstname, lastname, email, password, confirm_password } = req.body
+
+
+    // 2. check if the user already exists in db
+    db.oneOrNone('SELECT * FROM users WHERE email = $1;', [email])
+    .then(userExists => {
+        if (userExists){
+            // user exists, alert to prompt login
+            res.redirect('/login?message=User%20already%20exists.')
+        }
+        else{
+            const salt = bcrypt.genSaltSync(10);
+            const hash = bcrypt.hashSync(password, salt);
+    // 3. hash password - cleam the email
+            const cleanedEmail = email.toLowerCase().trim()
+
+            
+    // 4. insert into db
+            db.none('INSERT INTO users (firstname, lastname, email, password) VALUES ($1, $2, $3, $4)', [firstname, lastname, cleanedEmail, hash])
+            .then(()=>{
+                res.redirect('/login?message=User%20successfully%20created.')
+                
+            })
+            .catch((err)=>{
+                console.log(err);
+                res.json(err);
+            })   
+        }
+    })
+    .catch((err)=>{
+        console.log(err);
+        res.json(err);
+    }) 
+})
+
+module.exports = router
